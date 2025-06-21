fbcdeb634a998d2a3a83e73e6b38f5d23bbec5fe
Umaima Salam <umaimasalam2003@gmail.com>
2025-06-21 13:14:40 +0500

    Add files via upload

---
+++ assignment 4.js
@@ -0,0 +1,37 @@
+// 1. Declare 3 variables in one statement.
+// let x = 5 , y = 10 , z = 15;
+// console.log(x , y , z);
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+// LEGAL VARIABLE NAME:-
+// 1) userName
+// 2) _studentName
+// 3) $dollar
+// 4) currency_ruppess
+// 5) myVariable
+
+// LEGAL VARIABLE NAME:-
+// 1) 123name
+// 2) user Name
+// 3) if / else (reserved word)
+// 4) student-Name
+// 5) myVariable!
+
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// let a = ("Rules for naming JS variables");
+// let b = ("Variable names can only contain dollarsign($) ,underscore (_) ,camelCase  and  must start with letters. For example $my_1stVariable");
+// let c = ("Variables must begin with a letter,underscore(_) or Dollarsign($). For example $name, _name or name");
+// let d = ("Variable names are case sensitive");
+// let e= ("Variable names should not be JS keywords");
+// document.writeln("<h1>" , a, "</h1>", "<br>");
+// document.writeln(b, "<br>");
+// document.writeln(c, "<br>");
+// document.writeln(d, "<br>");
+// document.writeln(e, "<br>");
+
+
+
+
